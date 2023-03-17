84c717bc29942522ffbd5bc44e09c849b07c76a0
sydneychambers <116694729+sydneychambers@users.noreply.github.com>
2023-03-17 11:52:19 -0500

    Advisor dashboard and other related displays

---
+++ AdvisorClient/src/view/VCDisplay.java
@@ -0,0 +1,121 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class VCDisplay {
+    JFrame frame = new DefaultFrame();
+    JPanel mainPanel;
+    JPanel statusPanel;
+    JPanel selectionPanel;
+    JPanel selectionPanel2;
+    JPanel videoChatPanel;
+    JButton acceptCall;
+    JButton declineCall;
+    JButton returnBtn;
+    JInternalFrame videoChatWindow;
+    JRadioButton videoCallOnline;
+    JRadioButton videoCallOffline;
+    ButtonGroup statusButtonGroup;
+    JComboBox<String> pendingRequests;
+    String[] requests;
+
+    public VCDisplay() {
+        initialiseComponents();
+        addActionListeners();
+        addComponents();
+    }
+
+    private void initialiseComponents() {
+        //Initialise the panels and define layouts
+        mainPanel = new JPanel(new BorderLayout());
+        statusPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        statusPanel.add(new JLabel("Video Call Availability"));
+        selectionPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        selectionPanel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        videoChatPanel = new JPanel(new BorderLayout());
+
+        //Initialise the internal frame
+        videoChatWindow = new JInternalFrame("Live Call with X", true, false, true, true);
+        videoChatWindow.setPreferredSize(new Dimension(640, 480));
+        videoChatWindow.setVisible(true);
+
+        //Initialise the combo box
+        requests = new String[]{"Student 1", "Student 2", "Student 3"};
+        pendingRequests = new JComboBox<String>(requests);
+        pendingRequests.setEditable(false);
+
+        //Initialise the buttons
+        acceptCall = new JButton("Accept");
+        declineCall = new JButton("Decline");
+        returnBtn = new JButton("Return to Dashboard");
+        videoCallOnline = new JRadioButton("Online");
+        videoCallOffline = new JRadioButton("Offline");
+        statusButtonGroup = new ButtonGroup();
+    }
+
+    private void addActionListeners() {
+        videoCallOnline.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Enable the components when videoCallOnline is selected
+                pendingRequests.setEnabled(true);
+                acceptCall.setEnabled(true);
+                declineCall.setEnabled(true);
+            }
+        });
+
+        videoCallOffline.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Disable the components when videoCallOffline is selected
+                pendingRequests.setEnabled(false);
+                acceptCall.setEnabled(false);
+                declineCall.setEnabled(false);
+            }
+        });
+
+        returnBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new AdvisorDashboard();
+                frame.dispose();
+            }
+        });
+    }
+
+    private void addComponents() {
+        statusButtonGroup.add(videoCallOnline);
+        statusButtonGroup.add(videoCallOffline);
+        videoCallOffline.setSelected(true);
+
+        statusPanel.add(videoCallOnline);
+        statusPanel.add(videoCallOffline);
+
+        selectionPanel.add(pendingRequests);
+
+        selectionPanel2.add(returnBtn);
+        selectionPanel2.add(acceptCall);
+        selectionPanel2.add(declineCall);
+
+        videoChatPanel.add(videoChatWindow, BorderLayout.CENTER);
+        videoChatPanel.add(selectionPanel2, BorderLayout.SOUTH);
+
+        // Disable the components initially
+        pendingRequests.setEnabled(false);
+        acceptCall.setEnabled(false);
+        declineCall.setEnabled(false);
+
+
+
+        mainPanel.add(statusPanel, BorderLayout.NORTH);
+        mainPanel.add(selectionPanel, BorderLayout.SOUTH);
+        mainPanel.add(videoChatPanel, BorderLayout.CENTER);
+
+        frame.add(mainPanel);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new VCDisplay();
+    }
+}
